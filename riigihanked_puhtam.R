882bef164405f8238d4dc0747ef14ad68a57697e
Risto Hinno <ristohinno@gmail.com>
2016-08-11 15:45:21 +0300

    arendamine

--- riigihanked_puhtam.R
+++ riigihanked_puhtam.R
@@ -1,5 +1,6 @@
 data=read.csv("rhravaandmed.csv", sep=";")
-data_min=data[grepl("ministeerium$", data$Hankija),]
+#data_min=data[grepl("ministeerium$", data$Hankija),]
+data_min=data
 
 #abimuuujad
 library(rvest)
@@ -9,9 +10,10 @@ tegelik_maksumus=c()
 periood=c()
 cpv=c()
 #perioodi selectorid, mis loobib läbi
-periood_selectors=c("#If_90", "#If_124","#If_126", "#If_128","#If_130", "#If_132",
-                    "#If_134","#If_136","#If_138","#If_140","#If_144","#If_146",
-                    "#If_150","#If_152","#If_164","#If_174","#If_186","#If_210")
+periood_selectors=c("#If_90", "#If_124","#If_126", "#If_128","#If_130", 
+                    "#If_132","#If_134","#If_136","#If_138","#If_140",
+                    "#If_144","#If_146","#If_148", "#If_150","#If_152",
+                    "#If_164","#If_174","#If_186","#If_210")
 #funktsioon lihtsaks kraapimiseks selectori lõikes
 kraapija=function(selector, page=leht) {
   if(length(page %>% 
@@ -67,3 +69,30 @@ for (i in 1:50) { #tuleb käsitsi vaadata, kas ehk on juurde tulnud
 #esmane puhas
 esmane_sodine=data.frame(cpv, eeldatav_maksumus,lepingu_maksumus, 
                            tegelik_maksumus,periood)
+#eemaldame maksumusest sodi
+esmane_sodine[,2:4]=apply(esmane_sodine[,2:4], 2, 
+      function(y) gsub("EUR|-|Contractduration", "", y))
+#teeme perioodi korda, kestvuse paneme kuudes
+#teeme loopi
+tulem=c()
+library(lubridate)
+for(i in 1:nrow(esmane_sodine)) {
+  #kuudes
+  if(grepl("months",esmane_sodine$periood[i])) {
+    temp=gsub("\\smonths","",esmane_sodine$periood[i])
+    tulem[i]=temp
+  } #päevades
+  else if (grepl("days",esmane_sodine$periood[i])) {
+    temp=gsub("\\sdays","",esmane_sodine$periood[i])
+    temp=as.numeric(temp)/30
+    tulem[i]=temp
+  } #kpvde vahemikuna
+  else if (grepl("[0-9]{2,}.[0-9]{2,}.[0-9]{4,}.*-.*[0-9]{2,}.[0-9]{2,}.[0-9]{4,}", 
+                 esmane_sodine$periood[i])) {
+    temp=strsplit(as.character(esmane_sodine$periood[i]), split="-")
+    tulem[i]=difftime(dmy(temp[[1]][2]),dmy(temp[[1]][1]),
+                                      units="days")/30
+  }
+}
+#paneme algsesse juurde
+esmane_sodine$kestvus_paevades=tulem
